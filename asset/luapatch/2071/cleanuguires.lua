9535bc28e64f96fc2fb6ec8780605b0f55cd0123
ZeroRin <pptxp@qq.com>
2022-06-11 22:51:12 +0800

    add auto commit

--- asset/luapatch/2071/cleanuguires.lua
+++
@@ -1,8 +0,0 @@
-local util = require 'xlua.util'
-xlua.private_accessible(CS.CleanUGUIRes)
-
-local OnDestroy = function(self)
-	CS.ResManager.ResClean();
-end
-
-util.hotfix_ex(CS.CleanUGUIRes,'OnDestroy',OnDestroy)
--- asset/luapatch/2090/homemedalcontroller.lua
+++
@@ -1,7 +0,0 @@
-local util = require 'xlua.util'
-xlua.private_accessible(CS.HomeMedalController)
-local InitData_New = function(self, ...)
-	self.OpenEffect = false
-    self:InitData(...)
-end
-util.hotfix_ex(CS.HomeMedalController,'InitData',InitData_New)
