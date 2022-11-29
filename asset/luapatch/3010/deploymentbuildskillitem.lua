826ad9c8b81c023105f7156fc97beea9f87ec4ce
ZeroRin <ZeroRin@users.noreply.github.com>
2022-11-29 05:38:36 +0000

    [CH] 30100 | data 51cbf58 | dabao 20221129_11_38

--- asset/luapatch/3010/deploymentbuildskillitem.lua
+++ asset/luapatch/3010/deploymentbuildskillitem.lua
@@ -1,11 +1,30 @@
 local util = require 'xlua.util'
 xlua.private_accessible(CS.DeploymentBuildSkillItem)
+xlua.private_accessible(CS.DeploymentFriendlyTeamController)
+xlua.private_accessible(CS.DeploymentSangvisTeamController)
 
 local RefreshLeftUI = function(self)
 	self:RefreshLeftUI();
 	if self.skillTipTrans.gameObject.activeInHierarchy then
 		self:ShowSkillTipContent();
 	end
+	self:RefreshUI();
 end
 
+local SelectSkillSpot = function(self,spot)
+	if CS.UnityEngine.Input.touchCount >1 then
+		return;
+	end
+	self:CloseAllSelectTarget();	
+	self:SelectSkillSpot(spot);
+end
+
+local ControlBuild = function(self)
+	if CS.UnityEngine.Input.touchCount >1 then
+		return;
+	end
+	self:ControlBuild();
+end
 util.hotfix_ex(CS.DeploymentBuildSkillItem,'RefreshLeftUI',RefreshLeftUI)
+util.hotfix_ex(CS.DeploymentBuildSkillItem,'SelectSkillSpot',SelectSkillSpot)
+util.hotfix_ex(CS.DeploymentBuildSkillItem,'ControlBuild',ControlBuild)
