a73e8bd8c2a5e8b9a0a4e140b446fcd24a2e67d1
ZeroRin <ZeroRin@users.noreply.github.com>
2023-02-17 05:48:44 +0000

    [CH] 30100 | data 6e2f303 | dabao 20230216_22_19

--- asset/luapatch/3010/opspanelmissionbase.lua
+++ asset/luapatch/3010/opspanelmissionbase.lua
@@ -41,8 +41,21 @@ local InitData = function(self,data)
 			end);
 	end
 end
+
+local currentMissionInfo = function(self)
+	if CS.GameData.listMissionInfo:ContainsKey(self.CurrentMissionId) then
+		local info = CS.GameData.listMissionInfo:GetDataById(self.CurrentMissionId);
+		if info == nil then
+			print("缺少missioninfo"..self.CurrentMissionId);
+		end
+		return info;		
+	end
+	print("缺少missioninfo"..self.CurrentMissionId);
+	return nil;
+end
 util.hotfix_ex(CS.OPSPanelMissionBase,'RefreshUI',RefreshUI)
 util.hotfix_ex(CS.OPSPanelMissionBase,'InitData',InitData)
+--util.hotfix_ex(CS.OPSPanelMissionBase,'get_currentMissionInfo',currentMissionInfo)
 
 
 
