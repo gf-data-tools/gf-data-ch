7a4f334666b9537f32b3f26e5136bc0af8d7a44b
ZeroRin <ZeroRin@users.noreply.github.com>
2023-04-27 13:27:28 +0000

    CH | 30200 | data 2d0db5f | dabao 20230427_20_28

--- asset/luapatch/3020/playerreturneventctrl.lua
+++ asset/luapatch/3020/playerreturneventctrl.lua
@@ -4,7 +4,7 @@ xlua.private_accessible(CS.PlayerReturnEventCtrl)
 local Awake = function(self)
 	self:Awake();
 	if CS.HotUpdateController.instance.mUsePlatform == CS.HotUpdateController.EUsePlatform.ePlatform_Tw or CS.HotUpdateController.instance.mUsePlatform == CS.HotUpdateController.EUsePlatform.ePlatform_Korea then
-		local imageTrans1 = self.transform:Find("FundsContent\ActivateCard\Img_Bg");
+		local imageTrans1 = self.transform:Find("FundsContent/ActivateCard/Img_Bg");
 		if imageTrans1~= nil then
 			local obj1 = CS.ResManager.GetObjectByPath("AtlasClips3010/回归基金");
 			imageTrans1:GetComponent(typeof(CS.ExImage)).sprite = obj1:GetComponent(typeof(CS.UnityEngine.UI.Image)).sprite;
