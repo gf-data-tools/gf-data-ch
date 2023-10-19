7ca35d3c25c5d460685d842d198ee74ddcff7c07
ZeroRin <ZeroRin@users.noreply.github.com>
2023-10-19 08:19:26 +0000

    CH | 30300 | data 69557b7 | dabao 20231019_15_25

--- asset/luapatch/3030/logincontroller.lua
+++ asset/luapatch/3030/logincontroller.lua
@@ -1,35 +1,45 @@
 local util = require 'xlua.util'
 xlua.private_accessible(CS.LoginController)
-xlua.private_accessible(CS.AIhelpService)
+--xlua.private_accessible(CS.AIhelpService)
 
 local mReportUpload = function(self)
 	self:Start()
+	print("mReportUpload")
 	local plat = CS.ApplicationConfigData.PlatformChannelId;
 	if plat == "ios" or plat == "GWGW" or plat == "GWHF" or plat == "TapTap" then
-		-- 线上 最新非强更包 3.0302
-		if CS.ConfigData.clientVersion == "3.0301" then
+		local appId_new = "SunbornNetwork_platform_614373e1-b293-4130-a752-7d190897ba8f";
 
+		if CS.ApplicationConfigData.PlatformChannelId == "ios" then
+			appId_new = "SunbornNetwork_platform_35917ad1-7e61-4193-ac0a-9535295da641";
 		else
-			--线上 老包
-			print("线上 老包"..CS.ConfigData.clientVersion)
-			if CS.AIHelpSystem.instance ~=nil then
-				CS.UnityEngine.Object.DestroyImmediate(CS.AIHelpSystem.instance.gameObject);
-				CS.AIHelpSystem.instance=nil; 
-				print("delete old object")
-			end 
-			local obj = CS.UnityEngine.Object.Instantiate(CS.UnityEngine.Resources.Load("UGUIPrefabs/System/AIHelpSystem"));
-			obj.name = "AiHelpSystem2"
-		end	
+			appId_new = "SunbornNetwork_platform_614373e1-b293-4130-a752-7d190897ba8f";
+		end 
+		local appKey_new = "SUNBORNNETWORK_app_1d1709716ce14af4a1cc10831e839470";
+		local domain_new = "SunbornNetwork.aihelpcn.net";
+		--print(appId_new);
+		--print(appKey_new);
+		--print(domain_new);
+		CS.AIhelpService.Initialize(appKey_new, domain_new, appId_new);
 	end
 end
 local mInitialize = function(self,appkey,domain,appId)
+	local appId_new = "SunbornNetwork_platform_614373e1-b293-4130-a752-7d190897ba8f";
+
+	if CS.ApplicationConfigData.PlatformChannelId == "ios" then
+		appId_new = "SunbornNetwork_platform_35917ad1-7e61-4193-ac0a-9535295da641";
+	else
+		appId_new = "SunbornNetwork_platform_614373e1-b293-4130-a752-7d190897ba8f";
+	end 
+	local appKey_new = "SUNBORNNETWORK_app_1d1709716ce14af4a1cc10831e839470";
 	local domain_new = "SunbornNetwork.aihelpcn.net";
 	print(domain_new);
-	CS.AIhelpService._instance = CS.AIhelpService(appkey, domain_new, appId);
+	--CS.AIhelpService._instance = CS.AIhelpService(appKey_new, domain_new, appId_new);
+	CS.AIhelpService.Initialize(appKey_new, domain_new, appId_new);
 end
 if CS.HotUpdateController.instance.mUsePlatform == CS.HotUpdateController.EUsePlatform.ePlatform_Normal then 
     --util.hotfix_ex(CS.LoginController,'Start',mReportUpload)
     if CS.ApplicationConfigData.PlatformChannelId == "ios" or CS.ApplicationConfigData.PlatformChannelId == "TapTap" or CS.ApplicationConfigData.PlatformChannelId == "GWGW" or CS.ApplicationConfigData.PlatformChannelId == "GWHF" then
+		util.hotfix_ex(CS.LoginController,'Start',mReportUpload)
 		util.hotfix_ex(CS.AIhelpService,'Initialize',mInitialize)
 	end
 end
