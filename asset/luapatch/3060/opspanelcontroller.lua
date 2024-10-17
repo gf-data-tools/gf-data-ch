085d4f6f4da77dc7ddd1619854d3394c636d0aa5
ZeroRin <ZeroRin@users.noreply.github.com>
2024-10-17 02:49:32 +0000

    CH | 30600 | data 0d96912 | dabao 20241016_20_58

--- asset/luapatch/3060/opspanelcontroller.lua
+++ asset/luapatch/3060/opspanelcontroller.lua
@@ -22,9 +22,26 @@ local ShowItemLimitUINew = function(self,itemids)
 			local language = CS.Data.GetLang(60826);
 			local tipText = CS.System.String.Format(language, tostring(num1));
 			tip.strIntroduction = tipText;
+			tip.strTitle = itemabout.iteminfo.name;
 		end
 	end
 end
 
+local InitPanelSpot = function(self,spot)
+	self.panelSpot = spot;
+	self.mission = spot.mission;
+	self.opsMission = spot.opsMission;
+	self.missionInfo = spot.missionInfo;
+	self.entranceId = spot.opsMission.entranceId;
+	self:RefreshEntranceUI();
+	self:RefresCommonUI();
+	if self.opsMission ~= nil then
+		self:ShowNewReward();
+	else
+		self:ShowOldReward();
+	end
+	self:CheckInfoPos();
+end
 util.hotfix_ex(CS.OPSPanelController,'ShowItemLimitUINew',ShowItemLimitUINew)
+util.hotfix_ex(CS.SpecialMissionInfoController,'InitPanelSpot',InitPanelSpot)
 
